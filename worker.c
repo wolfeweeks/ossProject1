dd783785e8eb5329a5fa5f22bbef35c392bae433
Wolfe Weeks <wwwkf9@umsystem.edu>
2023-01-26 20:56:35 -0600

    step 4: implement worker.c iteration loop

--- worker.c
+++ worker.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
+/**
+ * @file worker.c
+ * @author Wolfe Weeks
+ * @date 2023-01-26
+ */
 
-int main() {
-  printf("Hello, world!\n");
+int main(int argc, char* argv[]) {
+
+  // check if command line argument is passed
+  if (argc != 2) {
+    printf("Error: missing command line argument for number of iterations\n");
+    return 1;
+  }
+
+  // convert command line argument to integer
+  int iterations = atoi(argv[1]);
+
+  // loop for the specified number of iterations
+  for (int i = 1; i <= iterations; i++) {
+    printf("WORKER PID: %d PPID: %d Iteration: %d before sleeping\n", getpid(), getppid(), i);
+    sleep(1);
+    printf("WORKER PID: %d PPID: %d Iteration: %d after sleeping\n", getpid(), getppid(), i);
+  }
+
   return 0;
 }
