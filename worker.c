eb8a01f68490e6437fce559fc3cc2b881707a5ab
Wolfe Weeks <wwwkf9@umsystem.edu>
2023-02-07 19:13:39 -0600

    updated for loop in worker.c

--- worker.c
+++ worker.c
@@ -16,7 +16,8 @@ int main(int argc, char* argv[]) {
   int iterations = atoi(argv[1]);
 
   // loop for the specified number of iterations
-  for (int i = 1; i <= iterations; i++) {
+  int i;
+  for (i = 1; i <= iterations; i++) {
     printf("WORKER PID: %d PPID: %d Iteration: %d before sleeping\n", getpid(), getppid(), i);
     sleep(1);
     printf("WORKER PID: %d PPID: %d Iteration: %d after sleeping\n", getpid(), getppid(), i);
